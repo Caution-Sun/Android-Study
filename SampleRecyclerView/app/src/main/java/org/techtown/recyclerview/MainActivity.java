11d5241d29918500ddd2004fec4284319f784512
Caution-Sun <hisbrjseka414>
2023-07-11 19:10:17 +0900

    RecyclerView(2)

--- SampleRecyclerView/app/src/main/java/org/techtown/recyclerview/MainActivity.java
+++ SampleRecyclerView/app/src/main/java/org/techtown/recyclerview/MainActivity.java
@@ -1,10 +1,13 @@
 package org.techtown.recyclerview;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.GridLayoutManager;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -15,14 +18,29 @@ public class MainActivity extends AppCompatActivity {
 
         RecyclerView recyclerView = findViewById(R.id.recyclerView);
 
-        LinearLayoutManager layoutManager = new LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false);
+        GridLayoutManager layoutManager = new GridLayoutManager(this, 2);
         recyclerView.setLayoutManager(layoutManager);
         PersonAdapter adapter = new PersonAdapter();
 
         adapter.addItem(new Person("AAA", "010-1111-1111"));
         adapter.addItem(new Person("BBB", "010-2222-2222"));
         adapter.addItem(new Person("CCC", "010-3333-3333"));
+        adapter.addItem(new Person("DDD", "010-4444-4444"));
+        adapter.addItem(new Person("EEE", "010-5555-5555"));
+        adapter.addItem(new Person("FFF", "010-6666-6666"));
+        adapter.addItem(new Person("GGG", "010-7777-7777"));
+        adapter.addItem(new Person("HHH", "010-8888-8888"));
+        adapter.addItem(new Person("III", "010-9999-9999"));
+        adapter.addItem(new Person("JJJ", "010-0000-0000"));
 
         recyclerView.setAdapter(adapter);
+
+        adapter.setOnItemClickListener(new OnPersonItemClickListener() {
+            @Override
+            public void onItemClick(PersonAdapter.ViewHolder holder, View view, int position) {
+                Person item = adapter.getItem(position);
+                Toast.makeText(getApplicationContext(), "아이템 선택됨 : " + item.getName(), Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
--- SampleRecyclerView/app/src/main/java/org/techtown/recyclerview/PersonAdapter.java
+++ SampleRecyclerView/app/src/main/java/org/techtown/recyclerview/PersonAdapter.java
@@ -10,8 +10,9 @@ import androidx.recyclerview.widget.RecyclerView;
 
 import java.util.ArrayList;
 
-public class PersonAdapter extends RecyclerView.Adapter<PersonAdapter.ViewHolder>{
+public class PersonAdapter extends RecyclerView.Adapter<PersonAdapter.ViewHolder> implements OnPersonItemClickListener{
     ArrayList<Person> items = new ArrayList<Person>();
+    OnPersonItemClickListener listener;
 
     @NonNull
     @Override
@@ -19,7 +20,7 @@ public class PersonAdapter extends RecyclerView.Adapter<PersonAdapter.ViewHolder
         LayoutInflater inflater = LayoutInflater.from(parent.getContext());
         View itemView = inflater.inflate(R.layout.person_item, parent, false);
 
-        return new ViewHolder(itemView);
+        return new ViewHolder(itemView, this);
     }
 
     @Override
@@ -49,15 +50,37 @@ public class PersonAdapter extends RecyclerView.Adapter<PersonAdapter.ViewHolder
         return items.size();
     }
 
+    public void setOnItemClickListener(OnPersonItemClickListener listener) {
+        this.listener = listener;
+    }
+
+    @Override
+    public void onItemClick(ViewHolder holder, View view, int position) {
+        if(listener != null){
+            listener.onItemClick(holder, view, position);
+        }
+    }
+
     static class ViewHolder extends RecyclerView.ViewHolder {
         TextView textView;
         TextView textView2;
 
-        public ViewHolder(@NonNull View itemView) {
+        public ViewHolder(@NonNull View itemView, final OnPersonItemClickListener listener) {
             super(itemView);
 
             textView = itemView.findViewById(R.id.textView);
             textView2 = itemView.findViewById(R.id.textView2);
+
+            itemView.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    int position = getAdapterPosition();
+
+                    if(listener != null){
+                        listener.onItemClick(ViewHolder.this, v, position);
+                    }
+                }
+            });
         }
 
         public void setItem(Person item){
